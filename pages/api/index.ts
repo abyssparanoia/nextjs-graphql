9676e68afef9e59abeabb64d46ef81a01cf50155
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-09-29 22:49:27 +0900

    feat: impl resolver based generate graphql

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,36 +1,65 @@
 import { ApolloServer, gql } from "apollo-server-micro";
+import {
+  MutationResolvers,
+  QueryResolvers,
+  Resolvers
+} from "../../lib/graphql/graphql-resolver-types";
+import fs from "fs";
+import path from "path";
 
-// ここからGraphQLのモックの定義
-const typeDefs = gql`
-  type Query {
-    hello: String
-    users: [User]
-  }
-  type User {
-    id: Int!
-    name: String!
-  }
-`;
+const typeDefs = fs
+  .readFileSync(path.join(__dirname, "../../lib/graphql/schema.graphql"))
+  .toString();
 
-const users = [
+interface User {
+  id: string;
+  name: string;
+}
+
+const users: User[] = [
   {
-    id: 1,
+    id: "hoge",
     name: "tarou"
   },
   {
-    id: 2,
+    id: "fuga",
     name: "zirou"
+  },
+  {
+    id: "fizz",
+    name: "saburou"
   }
 ];
 
-const resolvers = {
-  Query: {
-    hello: () => "world",
-    users: () => users
+const Query: QueryResolvers = {
+  user(_parent, args, _context, _info) {
+    return users.find((user) => user.id === args.id) || null;
+  },
+  users() {
+    return users;
   }
 };
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const Mutation: MutationResolvers = {
+  addUser(_parent, args, _context, _info) {
+    const newUser: User = {
+      id: `ID____${args.name}`,
+      name: args.name
+    };
+    users.push(newUser);
+    return newUser;
+  }
+};
+
+const resolvers: Resolvers = {
+  Query,
+  Mutation
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers: resolvers as any
+});
 
 export default apolloServer.createHandler({ path: "/api" });
 export const config = {
