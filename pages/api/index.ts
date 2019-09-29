4787c4c8ef3d988a05ed2b6744b3e35b1d92953f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-09-29 16:28:59 +0900

    feat: add graphql server

---
+++ pages/api/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+
+// ここからGraphQLのモックの定義
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export default apolloServer.createHandler({ path: "/api" });
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
